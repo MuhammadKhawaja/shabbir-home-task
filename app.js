a37892a8ef4fcbf0aa2acc0c06a0e90ac299127d
Muhammad Khawaja <mdkhawaja2427@gmail.com>
2023-09-08 05:22:51 -0700

    two files

---
+++ app.js
@@ -0,0 +1,143 @@
+// chapter 1 {Alerts}
+
+// question {1}
+
+var firstName="Muhammed"
+
+alert("firstName : " + firstName)
+
+// Q 1 {2}
+
+var lastName="Shabbir"
+
+alert("lastName : " + lastName)
+ 
+// q 1{3}
+
+var email="shabbir123@gmail.com"
+
+alert("email : " + email)
+
+// Q 1 {4}
+
+var phoneNumber="090078601"
+
+alert("phoneNumber : " + phoneNumber)
+
+// Q 1 {5}
+
+var password="********"
+
+alert("password : " + password)
+
+//  Q {2}
+
+alert("you're learning javascript")
+
+// Q {3}
+
+alert("Hello world......")
+
+// chapter 2 (variables for string)
+
+//Q {1} 
+
+var firstVariableName="Shabbir"
+
+// Q {2}
+
+var book ;
+
+book ="class one book "
+
+// Q {3}
+
+var teamName ="white house "
+
+alert(teamName)
+
+// Q {4}
+
+var bestMan ="charlie"
+
+bestMan ="Homelander"
+
+//chapter 3 (Home Task Variables For Numbers) 
+
+// Q {1}
+
+var caseQty;
+
+// Q [2]
+
+
+var caseQty;
+
+caseQty =144;
+
+// Q {3}
+
+var num = 9;
+
+// Q {4}
+
+var num;
+
+num = 4 + 4;
+
+// Q {5}
+
+var merchTotal = 100;
+
+var shippingCharge = 10;
+
+var orderTotal = merchTotal + shippingCharge;
+
+console.log (orderTotal);
+
+// Q {6}
+
+var num = 20;
+
+rum = num + 20;
+
+console.log(rum)
+
+// chapter 4 (variable names legal and illegal)
+ 
+// Q {1}
+
+var productCost = 3.45;
+
+// Q {2}
+
+var nameOfBand;
+
+// Q {3}
+
+var myMobileNumber = "980254554"
+
+// Q {4}
+
+var myfullName = "Muhammad Shabbir"
+
+// Q {5} legal variables
+
+var myFirstPlace
+
+var _variableName
+
+var myMobileNumber
+
+// Q{5} illegal variables
+
+
+console.log("user6name , username , 4name")
+
+
+
+
+// chapter 5 (math expression)
+
+
+// Q {1}
